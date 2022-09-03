d714032bbfec03b4f06129b13da39ce07550f488
Aleksey <rusal@bk.ru>
2022-09-03 19:24:49 +0300

    add stack class

--- TMP.cpp
+++ TMP.cpp
@@ -1,7 +1,24 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+void example_work_stack() {
+    std::cout << "Example work with stack!\n";
+    Stack st = Stack();
+    st.push(7);
+    st.push(3);
+    st.push(5);
+    st.push(4);
+    st.push(8);
+    int st_size = st.size();
+    cout << "Size of Stack " << st.size() << endl;
+    for (int i = 0; i < st_size; i++)
+        cout << st.pop() << endl;
+    cout << "Size of Stack " << st.size() << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    example_work_stack();
+    
 }
