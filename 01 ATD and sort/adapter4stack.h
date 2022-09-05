e85699d952ef58e28e86693663895570a85c9a7d
Aleksey <rusal@bk.ru>
2022-09-05 13:37:38 +0300

    add setElement func

--- 01 ATD and sort/adapter4stack.h
+++ 01 ATD and sort/adapter4stack.h
@@ -28,6 +28,28 @@ public:
 		}
 		return -1;
 	}
+	/// <summary>
+	/// Функция для изменения элемента стека по его индексу
+	/// </summary>
+	/// <param name="ind">индекс</param>
+	/// <param name="element">эелемент</param>
+	bool  setElement(int ind,int element) {
+		Stack* tmpSt1 = new Stack();
+		if ((ind >= 0) and (ind < st->size())) {
+			int size = st->size();
+			for (int i = 0; i < ind; i++) {
+				tmpSt1->push(st->pop());
+			}
+			tmpSt1->push(element);
+			for (int i = ind; i < size; i++) {
+				tmpSt1->push(st->pop());
+			}
+
+			return true;
+		}
+		
+		return false;
+	}
 	// Вспомогательная функция для добавления элемента `x` в stack
 	void push(int x)
 	{
@@ -35,7 +57,6 @@ public:
 	}
 	void display() {
 		Stack *tmpSt1 = new Stack();
-
 		int size = st->size();
 		for (int i = 0; i < size; i++) {
 			int element = st->pop();
@@ -46,8 +67,7 @@ public:
 		Stack* tmpSt2 = new Stack();
 		for (int i = 0; i < size; i++)
 			tmpSt2->push(tmpSt1->pop());
-		delete(tmpSt1);
-		
+		delete(tmpSt1);		
 		st = tmpSt2;
 		delete(tmpSt2);
 	}
