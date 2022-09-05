57762b2c7417dce5f43b2a263f47ef1314172699
Aleksey <rusal@bk.ru>
2022-09-05 12:04:32 +0300

    add element func stack adapter

--- 01 ATD and sort/TMP.cpp
+++ 01 ATD and sort/TMP.cpp
@@ -1,15 +1,16 @@
 ﻿#include <iostream>
 #include "Stack.h"
+#include "Adapter4stack.h"
 using namespace std;
 
 void example_work_stack() {
     std::cout << "Example work with stack!\n";
     Stack st = Stack();
-    st.push(137);
-    st.push(133);
-    st.push(135);
-    st.push(134);
-    st.push(138);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
     int st_size = st.size();
     cout << "Size of Stack " << st.size() << endl;
     for (int i = 0; i < st_size; i++){
@@ -18,12 +19,23 @@ void example_work_stack() {
     }
     cout << "Size of Stack " << st.size() << endl;
 }
+void example_work_adapter4stack() {
+    Adapter4stack* ast = new Adapter4stack();
+    ast->push(1);
+    ast->push(2);
+    ast->push(3);
+    ast->push(4);
+    ast->display();
+    cout << endl;
+    cout << ast->getElement(1);
 
+}
 
 
 int main()
 {
-    example_work_stack();
+    //example_work_stack();
+    example_work_adapter4stack();
     //ввод N
 
 
--- 01 ATD and sort/adapter4stack.h
+++ 01 ATD and sort/adapter4stack.h
@@ -17,16 +17,39 @@ public:
 	bool isFull() {
 		return st->isFull();     // или return size() == capacity;
 	}
+
 	int getElement(int ind) {
 		if ((ind >= 0) and (ind < st->size())){
 			int element;
-			for (int i = 0; i < ind; i++) {
+			for (int i = 0; i <= ind; i++) {
 				element = st->pop();
 			}
 			return element;
 		}
 		return -1;
 	}
+	// Вспомогательная функция для добавления элемента `x` в stack
+	void push(int x)
+	{
+		st->push(x);
+	}
+	void display() {
+		Stack *tmpSt1 = new Stack();
 
+		int size = st->size();
+		for (int i = 0; i < size; i++) {
+			int element = st->pop();
+			cout << " " << element;
+			tmpSt1->push(element);
+		}
+		delete(st);
+		Stack* tmpSt2 = new Stack();
+		for (int i = 0; i < size; i++)
+			tmpSt2->push(tmpSt1->pop());
+		delete(tmpSt1);
+		
+		st = tmpSt2;
+		delete(tmpSt2);
+	}
 };
 
