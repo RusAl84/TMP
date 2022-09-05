c969957a45cef5c29e5ab231ac2f409da36f581d
Aleksey <rusal@bk.ru>
2022-09-05 14:10:31 +0300

    add bubbleSort()

--- 01 ATD and sort/TMP.cpp
+++ 01 ATD and sort/TMP.cpp
@@ -36,11 +36,33 @@ void example_work_adapter4stack() {
     cout << ast->getElement(2);
 }
 
+void bubbleSort4stack() {
+    Adapter4stack* ast = new Adapter4stack();
+    ast->push(1);
+    ast->push(2);
+    ast->push(3);
+    ast->push(4);
+    ast->display();
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (ast->getElement(j) > ast->getElement(j+1)) {
+                int b = ast->getElement(j); // создали дополнительную переменную
+                ast->setElement(j, ast->getElement(j)); // digitals[j] = digitals[j + 1]; // меняем местами
+                ast->setElement(j + 1, b); //digitals[j + 1] = b; // значения элементов
+            }
+        }
+    }
+    cout << endl;
+    ast->display();
+    cout << endl;
+}
+
 
 int main()
 {
     //example_work_stack();
-    example_work_adapter4stack();
+    //example_work_adapter4stack();
+    bubbleSort4stack();
     //ввод N
 
 
