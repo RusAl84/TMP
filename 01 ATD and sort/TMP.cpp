cc23cfc6b0243639b90a4368941123c567deaba3
teacher <teacher@C239U00>
2022-09-10 13:34:31 +0300

    example_work_quickSortQueueMas

--- 01 ATD and sort/TMP.cpp
+++ 01 ATD and sort/TMP.cpp
@@ -176,49 +176,42 @@ void CompCountSort(int* array, int* count, int size)
 // var 45
 void example_work_queueMas() {
     QueueMas *q1 = new QueueMas();
-    q1->push(1);
-    q1->push(2);
-    q1->push(3);
-    q1->push(4);
-    q1->push(5);
+    q1->push(1000);
+    q1->push(2000);
+    q1->push(3000);
+    q1->push(4000);
+    q1->push(5000);
     int size = q1->size();
     cout << "Size of queue: " << size << endl;
     cout << endl; q1->Dislay();
-    cout << endl; q1->Dislay();
-    cout << endl; q1->Dislay();
-    cout << endl; q1->Dislay();
+    //cout << endl; q1->Dislay();
+    //cout << endl; q1->Dislay();
+    //cout << endl; q1->Dislay();
     cout << endl;
     for (int i = 0; i < size; i++) {
         cout << q1->front() << " ";
         q1->pop();
     }    
-    cout << endl;
+    cout << endl; q1->Dislay();
     q1->push(100);
-    q1->push(100);
-    q1->push(100);
-    q1->push(100);
-    q1->push(100);
-    q1->push(100);
-    q1->push(100);
-    q1->push(100);
-    q1->push(100);
-    q1->Dislay();
-
+    cout << endl; q1->Dislay();
+    q1->pop();
     size = q1->size();
+
     cout << endl << "Size of queue: " << size << endl;
 }
 void example_work_adapter4queueMas() {
     adapter4queueMas* q1 = new adapter4queueMas();
-    q1->push(1);
-    q1->push(2);
-    q1->push(3);
-    q1->push(4);
-    q1->push(5);
+    q1->push(1000);
+    q1->push(2000);
+    q1->push(3000);
+    q1->push(4000);
+    q1->push(5000);
     int size = q1->size();
     cout << "Size of queue: " << size << endl;
     cout << endl; q1->Dislay();
-    q1->setElement(4,1000);
-    int element = q1->getElement(4);
+    q1->setElement(3,9999);
+    int element = q1->getElement(3);
     cout << endl<< element;
     cout << endl; q1->Dislay();
     cout << endl; q1->Dislay();
@@ -546,6 +539,77 @@ void _adapter4stackMas() {
     cout << endl;
 }
 
+//29
+// Разделение по схеме Lomuto
+int partitionQueueMas(adapter4queueMas* a, int start, int end)
+{
+    // Выбираем крайний правый элемент в качестве опорного элемента массива
+    //int pivot = a[end];
+    int pivot = a->getElement(end);
+
+    // элементы, меньшие точки поворота, будут перемещены влево от `pIndex`
+    // элементы больше, чем точка поворота, будут сдвинуты вправо от `pIndex`
+    // равные элементы могут идти в любом направлении
+    int pIndex = start;
+
+    // каждый раз, когда мы находим элемент, меньший или равный опорному, `pIndex`
+    // увеличивается, и этот элемент будет помещен перед опорной точкой.
+    for (int i = start; i < end; i++)
+    {
+        //if (a[i] <= pivot)
+        if (a->getElement(i) <= pivot)
+        {
+            //swap(a[i], a[pIndex]);
+            int tmp = a->getElement(i);
+            a->setElement(i, a->getElement(pIndex));
+            a->setElement(pIndex, tmp);
+            pIndex++;
+        }
+    }
+
+    // поменять местами `pIndex` с пивотом
+    //swap(a[pIndex], a[end]);
+
+    int tmp = a->getElement(end);
+    a->setElement(end, a->getElement(pIndex));
+    a->setElement(pIndex, tmp);
+
+    // вернуть `pIndex` (индекс опорного элемента)
+    return pIndex;
+}
+// Процедура быстрой сортировки
+void quicksortQueueMas(adapter4queueMas* a, int start, int end)
+{
+    // базовое условие
+    if (start >= end) {
+        return;
+    }
+    // переставить элементы по оси
+    int pivot = partitionQueueMas(a, start, end);
+
+    // повторяем подмассив, содержащий элементы, меньшие опорной точки
+    quicksortQueueMas(a, start, pivot - 1);
+
+    // повторяем подмассив, содержащий элементы, превышающие точку опоры
+    quicksortQueueMas(a, pivot + 1, end);
+}
+void example_work_quickSortQueueMas() {
+    adapter4queueMas* q1 = new adapter4queueMas();
+    q1->push(1000);
+    q1->push(2000);
+    q1->push(3000);
+    q1->push(4000);
+    q1->push(5000);
+    int size = q1->size();
+    cout << "Size of queue: " << size << endl;
+    cout << endl; q1->Dislay();
+    quicksortQueueMas(q1, 0, size - 1);
+
+    cout << endl; q1->Dislay();
+};
+
+
+
 int main()
 {
     // var 7
@@ -562,8 +626,8 @@ int main()
     //example_work_quickSortQueuePtr();
 
     // var 38
-    example_work_stackMas();
-    example_work_adapter4stackMas();
+    //example_work_stackMas();
+    //example_work_adapter4stackMas();
 
     // var 44
     //example_work_stack();
@@ -584,5 +648,10 @@ int main()
     //example_work_stack();
     //example_work_adapter4stack();
     //mergeSort_start_adapter4stack();
+
+    // var 29
+    //example_work_queueMas();
+    //example_work_adapter4queueMas();
+    //Быстрая сортировка Хоару(без медианного(pivot) элемента)
 }
 
