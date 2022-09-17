4feccb60fafe4520e12d9ca29b3fa4076a8c6805
Aleksey <rusal@bk.ru>
2022-09-17 17:46:39 +0300

    rasschet

--- 01 ATD and sort/03_example_report2/example_report2/StackMas.h
+++ 01 ATD and sort/03_example_report2/example_report2/StackMas.h
@@ -9,54 +9,67 @@ class StackMas
     int top;
     int data[SIZE];
     int capacity;
-    bool debug;
+    int* nop;
 public:
     StackMas()
     {
         int size = SIZE;
         capacity = size;
         top = -1;
+        nop = nullptr;
     }
     ~StackMas() {
         //delete[] arr;
     }
-    void display() {
-        int st_size = size();
-        StackMas* st1 = new StackMas();
-        for (int i = 0; i < st_size; i++) {
-            st1->push(peek());
-            cout << " " << pop();
+    void setNOP(int* _nop) {
+        nop = _nop;
+    }
+    int* getNOP() {
+        return nop;
+    }
+    void display() { //2 + 4n
+        int st_size = size(); *nop += 1;
+        StackMas* st1 = new StackMas(); *nop += 1; //2
+        for (int i = 0; i < st_size; i++) { //2n
+            st1->push(peek()); *nop += 1; //2
+            cout << " " << pop(); *nop += 1;
         }
         cout << endl;
         for (int i = 0; i < st_size; i++) {
-            push(st1->pop());
+            push(st1->pop()); //2 
+            *nop += 2;
         }
     }
-    void push(int element)
+    void push(int element) //3
     {
+        *nop += 1;
         if (not isFull())
         {
-            top++;
-            data[top] = element;
+            top++; *nop += 1;
+            data[top] = element; *nop += 1;
         }
     }
-    int pop()
+    int pop() //3
     {
-        int element = data[top];
-        top--;
-        return element;
+        int element = data[top]; *nop += 1;
+        top--; *nop += 1; *nop += 1;
+        return element; 
     }
     int peek()
     {
+        *nop += 1;
         return data[top];
     }
     int size() {
+        *nop += 1;
         return top + 1;
     }
     bool isEmpty() {
+        *nop += 1;
         return top == -1;               
     }
     bool isFull() {
+        *nop += 1;
         return top == capacity - 1;     
     }
 };
--- 01 ATD and sort/03_example_report2/example_report2/example_report2.cpp
+++ 01 ATD and sort/03_example_report2/example_report2/example_report2.cpp
@@ -1,9 +1,7 @@
 ﻿#include <iostream>
 #include "StackMas.h"
 #include "Adapter4stackMas.h"
-
 using namespace std;
-
 void example_work_stackMas() {
     cout << "Example work with stack!\n";
     StackMas* st = new StackMas();
@@ -36,53 +34,103 @@ void example_work_adapter4stackMas() {
     cout << ast->getElement(2);
 }
 
-int partitionStackMas(Adapter4stackMas* a, int start, int end)
+int partitionStackMas(Adapter4stackMas* a, int start, int end, int* nop)
 {
-    int pivot = a->getElement(end);
-    int pIndex = start;
-    for (int i = start; i < end; i++)
+    int pivot = a->getElement(end);  *nop += 1;
+    int pIndex = start; *nop += 1;
+    for (int i = start; i < end; i++) 
     {
         if (a->getElement(i) <= pivot)
         {
-            int tmp = a->getElement(i);
-            a->setElement(i, a->getElement(pIndex));
-            a->setElement(pIndex, tmp);
-            pIndex++;
+            int tmp = a->getElement(i); *nop += 1;
+            a->setElement(i, a->getElement(pIndex)); *nop += 1;
+            a->setElement(pIndex, tmp); *nop += 1;
+            pIndex++; *nop += 1;
         }
     }
-    int tmp = a->getElement(end);
-    a->setElement(end, a->getElement(pIndex));
-    a->setElement(pIndex, tmp);
+    int tmp = a->getElement(end); *nop += 1;
+    a->setElement(end, a->getElement(pIndex)); *nop += 1;
+    a->setElement(pIndex, tmp); *nop += 1;
     return pIndex;
 }
 
-void quicksortStackMas(Adapter4stackMas* a, int start, int end)
+void quicksortStackMas(Adapter4stackMas* a, int start, int end, int* _nop)
 {
     if (start >= end) {
         return;
     }
-    int pivot = partitionStackMas(a, start, end);
-    quicksortStackMas(a, start, pivot - 1);
-    quicksortStackMas(a, pivot + 1, end);
+    int pivot = partitionStackMas(a, start, end, _nop);
+    quicksortStackMas(a, start, pivot - 1, _nop);
+    quicksortStackMas(a, pivot + 1, end, _nop);
 }
-void example_work_quickSortStackMas() {
+void example_work_quickSortStackMas(int* _nop) {
     Adapter4stackMas* st = new Adapter4stackMas();
-    st->push(1);
-    st->push(2);
-    st->push(3);
-    st->push(4);
-    st->push(5);
-    int size = st->size();
-    cout << "Size: " << size << endl;
-    cout << endl; st->display();
-    quicksortStackMas(st, 0, size - 1);
-    cout << endl << "sorted ";
-    cout << endl; st->display();
+    st->setNOP(_nop);
+    for (int n = 300; n <= 3000; n += 300)
+    {
+        int* p1 = st->getNOP();
+        *p1 = 0;
+        //int n = 300;
+        for (int i = 0; i < n; i++)
+            st->push(rand()); *_nop += 1;
+        int size = st->size(); *_nop += 1;
+        const clock_t begin_time = clock();
+        quicksortStackMas(st, 0, size - 1, _nop);
+        cout << endl << " n = " << n << " time (ms): ";
+        cout << endl;
+        cout << float(clock() - begin_time) / CLOCKS_PER_SEC;
+        cout << endl << " NOP: " << *st->getNOP() << endl;
+        for (int i = 0; i < n; i++)
+            st->pop();
+
+    }
 }
 int main()
 {
     // var 38
-    example_work_stackMas();
-    example_work_adapter4stackMas();
-    example_work_quickSortStackMas();
+    int* nop = new int;
+    *nop = 0;
+    //example_work_stackMas();
+    //example_work_adapter4stackMas();
+    example_work_quickSortStackMas(nop);
+
+    //    n = 300 time(ms) :
+    //    0.137
+    //    NOP : 59586104
+
+    //    n = 600 time(ms) :
+    //    0.529
+    //    NOP : 235660892
+
+    //    n = 900 time(ms) :
+    //    1.234
+    //    NOP : 674793880
+
+    //    n = 1200 time(ms) :
+    //    1.595
+    //    NOP : 1141864334
+
+    //    n = 1500 time(ms) :
+    //    2.743
+    //    NOP : 1990216642
+
+    //    n = 1800 time(ms) :
+    //    3.865
+    //    NOP : -1424239708
+
+    //    n = 2100 time(ms) :
+    //    5.443
+    //    NOP : -273528386
+
+    //    n = 2400 time(ms) :
+    //    7.226
+    //    NOP : 1343098684
+
+    //    n = 2700 time(ms) :
+    //    8.138
+    //    NOP : 2112128298
+
+    //    n = 3000 time(ms) :
+    //    10.891
+    //    NOP : 114136684
 }
