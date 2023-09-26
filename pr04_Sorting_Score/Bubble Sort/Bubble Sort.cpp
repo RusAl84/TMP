ae69b2aa29bb0367c120b68d1150ca22830aabe6
Aleksey <rusal@bk.ru>
2023-09-26 13:13:37 +0300

    fix bubblesort

--- pr04_Sorting_Score/Bubble Sort/Bubble Sort.cpp
+++ pr04_Sorting_Score/Bubble Sort/Bubble Sort.cpp
@@ -20,6 +20,27 @@ vector<int> genData(int n) {
     return arr;
 }
 
+unsigned int bubbleSort(vector<int> arr, int n)
+{
+    unsigned int nop = 0;
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false; nop++; // 1
+        for (j = 0; j < n - i - 1; j++) {
+            nop++; //1
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);  nop++; nop++;//2
+                swapped = true; nop++;//1
+            }
+        }
+        // If no two elements were swapped
+        // by inner loop, then break
+        //if (swapped == false)  nop++; //1
+        //    break;
+    }
+    return nop;
+}
 
 int main()
 {
@@ -34,8 +55,8 @@ int main()
         int N = presets[i];
         cout << "N = " << N;
         arr = genData(N);
-        //unsigned int nop = bubbleSort(arr, N);
-        //cout << " nop = " << nop << endl;
+        unsigned int nop = bubbleSort(arr, N);
+        cout << " nop = " << nop << endl;
     }
 
     return 0;
