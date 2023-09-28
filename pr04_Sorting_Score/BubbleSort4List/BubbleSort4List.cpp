6505a5556c10de25339c06de4e0d0df30ea6a705
Aleksey <rusal@bk.ru>
2023-09-28 15:20:55 +0300

    fix

--- pr04_Sorting_Score/BubbleSort4List/BubbleSort4List.cpp
+++ pr04_Sorting_Score/BubbleSort4List/BubbleSort4List.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+unsigned int nop = 0;
+
 class Adapter4List {
 private:
     list<int> *lis;
@@ -19,6 +21,12 @@ public:
     }
     void push(int data) {
         lis->push_front(data);
+    }
+    void clear() {
+        lis->clear();
+    }
+    int size() {
+        return lis->size();
     }
     void display() {
         list<int> *tlis = new list<int>();
@@ -42,10 +50,12 @@ public:
             element = lis->front();
             tlis->push_front(element);
             lis->pop_front();
+            nop += 3;
         }
         for (int i = 0; i < ind; i++) {
             lis->push_front(tlis->front());
             tlis->pop_front();
+            nop += 3;
         }
         return element;
     }
@@ -54,80 +64,77 @@ public:
         for (int i = 0; i < ind; i++) {
             tlis->push_front(lis->front());
             lis->pop_front();
+            nop += 3;
         }
         lis->pop_front();
         tlis->push_front(element);
         ind++;
+        nop += 3;
         for (int i = 0; i < ind; i++) {
             lis->push_front(tlis->front());
             tlis->pop_front();
+            nop += 3;
         }
+    }
+    void genData(int n) {
+    srand(unsigned(time(nullptr)));
+    for(int i=0;i<n;i++){
+        lis->push_front(rand());
+    }
+    //generate(arr.begin(), arr.end(), );
     }
 };
 
 
-
-//list<int> genData(int n) {
-//
-//    srand(unsigned(time(nullptr)));
-//    list<int> arr(n);
-//    generate(arr.begin(), arr.end(), rand);
-//    return arr;
-//}
-//
-//unsigned int bubbleSort(list<int> arr, int n)
-//{
-//    unsigned int nop = 0;
-//    int i, j;
-//    bool swapped;
-//    for (i = 0; i < n - 1; i++) {
-//        swapped = false; nop++; // 1
-//        for (j = 0; j < n - i - 1; j++) {
-//            nop++; //1
-//            if (arr[j] > arr[j + 1]) {
-//                swap(arr[j], arr[j + 1]);  nop++; nop++;//2
-//                swapped = true; nop++;//1
-//            }
-//        }
-//        // If no two elements were swapped
-//        // by inner loop, then break
-//        if (swapped == false)  nop++; //1
-//            break;
-//    }
-//    return nop;
-//}
+void bubbleSort(Adapter4List* a)
+{
+    int i, j;
+    int n = a->size();
+    for (i = 0; i < n - 1; i++) {
+        nop++; // 1
+        for (j = 0; j < n - i - 1; j++) {
+            nop++; //1
+            //if (arr[j] > arr[j + 1]) {
+            if (a->getElement(j) > a->getElement(j+1)) {
+                int tmp = a->getElement(j);
+                a->setElement(j, a->getElement(j + 1));
+                a->setElement(j+1, tmp);
+                nop+=3; //3
+            }
+        }
+    }
+}
 
 int main()
 {
-    list<int> arr;
-    
-    //printArray(arr); //Проверка генерации чисел
+
     cout <<  endl;
-    vector<int> presets = { 500, 1000, 3000, 5000, 8000, 10000, 20000, 30000 };
+    vector<int> presets = { 50, 100, 150, 200, 250, 300};
     
-
     Adapter4List *a = new Adapter4List();
-    a->push(1);
-    a->push(2);
-    a->push(3);
-    a->push(4);
-    a->push(5);
-    a->display();
-    cout << "\n" << a->getElement(0);
-    a->setElement(3, 777);
-    a->setElement(4, 888);
-    a->setElement(0, 111);
-    cout << "\n";
-    a->display();
+    //a->push(1);
+    //a->push(2);
+    //a->push(3);
+    //a->push(4);
+    //a->push(5);
+    //a->display();
+    //cout << "\n" << a->getElement(0);
+    //a->setElement(3, 777);
+    //a->setElement(4, 888);
+    //a->setElement(0, 111);
+    //cout << "\n";
+    //a->display();
 
-    //for (int i = 0; i < presets.size(); i++)
-    //{
-    //    int N = presets[i];
-    //    cout << "N = " << N;
-    //    arr = genData(N);
-    //    unsigned int nop = bubbleSort(arr, N);
-    //    cout << " nop = " << nop << endl;
-    //}
+    for (int i = 0; i < presets.size(); i++)
+    {
+        int N = presets[i];
+        cout << "N = " << N;
+        a->clear();
+        a->genData(N);
+        nop = 0;
+        bubbleSort(a);
+        cout << " nop = " << nop << endl;
+    }
 
     return 0;
 }
