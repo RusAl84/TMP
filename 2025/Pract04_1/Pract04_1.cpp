76ea5591dbd9b9d9b6fa1573a5477e4eb53ea27c
Huawei <Huawei@LAPTOP-FKEMLJA8>
2025-10-20 10:26:08 +0300

    final

--- 2025/Pract04_1/Pract04_1.cpp
+++ 2025/Pract04_1/Pract04_1.cpp
@@ -1,6 +1,8 @@
 ﻿// Pract04_1.cpp Вариант 80
 #include <stack>
 #include <iostream>
+#include <Windows.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -26,13 +28,36 @@ public:
 		return st->empty();
 	}
 	void display() {
-		stack <int>* tmp_st;
+		stack <int>* tmp_st = new stack <int>();
+		while (not st->empty()) {
+			int x = st->top();
+			st->pop();
+			cout << x << endl;
+			tmp_st->push(x);
+		}
+		while (not tmp_st->empty()) {
+			int x = tmp_st->top();
+			tmp_st->pop();
+			st->push(x);
+		}
 	}
 
 };
 
 int main()
 {
-   cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Вариант 80 практ. 4 ч1\n";
+	myStack st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.push(4);
+	st.display();
+	cout << "___\n";
+	st.display();
+	_getch();
 }
 
