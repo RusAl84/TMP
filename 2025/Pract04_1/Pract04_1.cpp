564e64ccca57a8cbe02248f605f9a837aed5f626
Huawei <Huawei@LAPTOP-FKEMLJA8>
2025-10-20 10:06:12 +0300

    start

---
+++ 2025/Pract04_1/Pract04_1.cpp
@@ -0,0 +1,38 @@
+﻿// Pract04_1.cpp Вариант 80
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+class myStack {
+private:
+	stack <int>* st;
+
+public:
+	myStack()
+	{ 
+		st = new stack <int>;
+	}
+	void push(int _element) {
+		st->push(_element);
+	}
+	void pop() {
+		st->pop();
+	}
+	int top() {
+		return st->top();
+	}
+	bool empty(){
+		return st->empty();
+	}
+	void display() {
+		stack <int>* tmp_st;
+	}
+
+};
+
+int main()
+{
+   cout << "Hello World!\n";
+}
+
