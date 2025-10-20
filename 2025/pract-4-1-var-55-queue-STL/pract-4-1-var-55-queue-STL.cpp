62ee703bd96d4d6af77af905ceb32f0deeda3f0b
Huawei <Huawei@LAPTOP-FKEMLJA8>
2025-10-20 15:45:59 +0300

    fix

---
+++ 2025/pract-4-1-var-55-queue-STL/pract-4-1-var-55-queue-STL.cpp
@@ -0,0 +1,23 @@
+﻿// pract-4-1-var-55-queue-STL.cpp 
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class myQueue {
+
+private:
+	queue <int>* q;
+public:
+	myQueue()
+	{
+		q = new queue<int>();
+	}
+
+
+};
+
+
+int main()
+{
+   cout << "queue<int>* STL\n";
+}
