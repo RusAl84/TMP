2dffd0d1a1178c07a703e030f230f69c2f8ee62e
Huawei <Huawei@LAPTOP-FKEMLJA8>
2025-10-22 17:39:03 +0300

    fix

---
+++ 2025/pract4-1-queue-array-var29/pract4-1-queue-array-var29.cpp
@@ -0,0 +1,100 @@
+﻿// pract4-1-queue-array-var29.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+using namespace std;
+
+class myQueue {
+
+    // Array to store queue elements.
+    int* arr;
+
+    // Maximum number of elements the queue can hold.
+    int capacity;
+
+    // Current number of elements in the queue.
+    int size;
+
+public:
+    myQueue(int c) {
+        capacity = c;
+        arr = new int[capacity];
+        size = 0;
+    }
+
+    bool isEmpty()
+    {
+        return size == 0;
+    }
+    bool isFull()
+    {
+        return size == capacity;
+    }
+
+    // Adds an element x at the rear of the queue.
+    void enqueue(int x)
+    {
+        if (isFull())
+        {
+            cout << "Queue is full!\n";
+            return;
+        }
+        arr[size] = x;
+        size++;
+    }
+
+    // Removes the front element of the queue.
+    void dequeue()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty!\n";
+            return;
+        }
+        for (int i = 1; i < size; i++)
+        {
+            arr[i - 1] = arr[i];
+        }
+        size--;
+    }
+
+    // Returns the front element of the queue.
+    int getFront()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty!\n";
+            return -1;
+        }
+        return arr[0];
+    }
+    // Return the last element of queue
+    int getRear()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty!" << endl;
+            return -1;
+        }
+        return arr[size - 1];
+    }
+
+
+};
+
+int main()
+{
+    myQueue q(3);
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    cout << "Front: " << q.getFront() << endl;
+
+    q.dequeue();
+    cout << "Front: " << q.getFront() << endl;
+    cout << "Rear: " << q.getRear() << endl;
+
+    q.enqueue(40);
+
+    return 0;
+}
